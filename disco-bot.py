35ae988f1c15642181ec74217fdde6f3acb091dc
Daniel Wu <daniel.wu.14@gmail.com>
2018-04-12 18:50:43 -0700

    fixed -nuke

--- disco-bot.py
+++ disco-bot.py
@@ -22,6 +22,8 @@ valid_commands = ['checkperm', 'wipe', 'logout', 'nuke', 'oppress', 'oyvey']
 @asyncio.coroutine
 def on_message(message):
     keywords = message.content.split(" ")
+    if keywords == []:
+        return
     if keywords[0][1:] not in valid_commands and keywords[0][0] == '-':
         client.send_message("Not a valid command, try -help")
     message_permissions = message.author.permissions_in(message.channel).manage_messages
@@ -39,12 +41,19 @@ def on_message(message):
 
         if message.content.startswith('-nuke'):
             if message_permissions:
-                keywords = message.content.split(" ")
                 if len(keywords) > 1:
-                    messages = yield from client.logs_from(message.channel, int(keywords[1]) + 1)
-                    for txt in messages:
-                        if not message.pinned:
-                            yield from client.delete_message(txt)
+                    not_pinned = lambda x: x.pinned != True
+                    deleted = yield from client.purge_from(message.channel, limit = int(keywords[1]) + 1, check = not_pinned)
+                else:
+                    yield from client.send_message(message.channel,'Format is: !nuke [number of messages to delete]')
+            else:
+                yield from client.send_message(message.channel, 'Does not have proper permissions!')
+
+        if message.content.startswith('-dronestrike'):
+            if message_permissions:
+                if len(keywords) > 1:
+                    not_pinned = lambda x: x.pinned != True
+                    deleted = yield from client.purge_from(message.channel, limit = int(keywords[1]) + 1, check = not_pinned)
                 else:
                     yield from client.send_message(message.channel,'Format is: !nuke [number of messages to delete]')
             else:
